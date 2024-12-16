e0935c74e1bab3ea7b3afafda8acbaa80bb3a266
Iana78uu <kuroozamkai0713@outlook.com>
2024-12-16 08:43:38 +0700

    menambahkan 3 fungsi pada m_contoh.php

--- models/M_contoh.php
+++ models/M_contoh.php
@@ -1,6 +1,18 @@
 <?php
 
 class m_contoh{
+    public function tampil() 
+    {
+        //fungsi untuk menampilkan data dari database
+    }
+    function edit () 
+    {
+        //fungsi untuk edit data dari database
+    }
+    function hapus() 
+    {
+        //fungsi untuk menghapus data dari database
+    }
 
 }
 
